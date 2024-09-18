540516d217506cf8e608405b254e26772a6286a7
V993 <V99352@gmail.com>
2024-09-17 22:56:51 -0500

    LONI tutorial

---
+++ batch_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -A loni_deepvote2
+#SBATCH -p gpu
+#SBATCH -t 72:00:00
+#SBATCH -N 1
+#SBATCH -n 48
+#SBATCH --mail-type END
+#SBATCH --mail-type FAIL
+#SBATCH --mail-user lmatone@tulane.edu
+
+module load conda
+cd $SLURM_SUBMIT_DIR
+source activate testing
+
+./start dark_knight.txt "the figure emerges"
+echo "./start dark_knight.txt “the figure emerges"
+
+exit
